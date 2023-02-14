1747e88199054d8ef16fc1d513a83a32f710d58d
mitesh582 <miteshjethava001@gmail.com>
2023-02-14 16:19:17 +0530

    action

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './container/Header/Header';
+import Counter from './componats/Counter/Counter';
+
+function App(){
+    return(
+        <>
+            <Header />
+            <Counter />
+        </>
+    )
+}
+
+export default App;
